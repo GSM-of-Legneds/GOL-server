952fe52378a40eb519a95b403f109ba88ac2a107
양시준 <80192911+YangSiJun528@users.noreply.github.com>
2023-07-03 01:18:08 +0900

    Summoner가 User 객체 대신 userId를 가지도록 변경 (#4)

--- common/src/main/kotlin/dev/yangsijun/gol/common/entity/statistics/Statistics.kt
+++ common/src/main/kotlin/dev/yangsijun/gol/common/entity/statistics/Statistics.kt
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.index.CompoundIndex
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-@CompoundIndex(name = "cmp-idx-by_id", def = "{'summoners.summoner.user.id': 1, 'summoners.id': 1, 'createdDate': -1}", unique = true)
+@CompoundIndex(name = "cmp-idx-by_id", def = "{'summoners.summoner.userId': 1, 'summoners.id': 1, 'createdDate': -1}", unique = true)
 @CompoundIndex(name = "cmp-idx-by_data", def = "{'createdDate': -1}", unique = true)
 class Statistics(
     @Id var id: ObjectId? = null,
