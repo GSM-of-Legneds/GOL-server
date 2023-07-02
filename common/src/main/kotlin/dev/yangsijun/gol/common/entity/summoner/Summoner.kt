2bc290a51a34106a291bb2a39cd61abad693b135
YangSijun528 <yangsijun5528@gmail.com>
2023-07-03 01:13:35 +0900

    Summoner가 User 객체 대신 userId를 가지도록 변경

--- common/src/main/kotlin/dev/yangsijun/gol/common/entity/summoner/Summoner.kt
+++ common/src/main/kotlin/dev/yangsijun/gol/common/entity/summoner/Summoner.kt
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 @Document
 class Summoner(
     @Id var id: ObjectId? = null,
-    @DBRef(lazy = false) val user: User,
+    val userId: ObjectId,
     val summonerId: String,
     val accountId: String,
     val puuid: String,
