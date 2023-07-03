1988dbd62601ba94dd573d9af3f0d5889daacb7b
YangSijun528 <yangsijun5528@gmail.com>
2023-07-03 19:21:27 +0900

    webflux 의존성 제거하고 web 의존성 추가
    WebClient를 사용하기 어려워서 RestTemplete으로 API 호출하도록 변경

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {}
 
 dependencies {
     /* api */
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
