6274eaeaa7426560cc9e82efbfdfdc6a73418e4c
YangSijun528 <yangsijun5528@gmail.com>
2023-07-07 15:08:10 +0900

    로그인 성공/실패 시 특정 FE 주소로 이동

--- api/src/main/kotlin/dev/yangsijun/gol/api/global/security/SecurityConfig.kt
+++ api/src/main/kotlin/dev/yangsijun/gol/api/global/security/SecurityConfig.kt
@@ -2,10 +2,13 @@ package dev.yangsijun.gol.api.global.security
 
 import dev.yangsijun.gauth.autoconfigure.EnableGAuth
 import dev.yangsijun.gauth.configurer.GAuthLoginConfigurer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
 
 
 @EnableGAuth
@@ -13,6 +16,10 @@ import org.springframework.security.web.SecurityFilterChain
 class SecurityConfig(
     val gauth: GAuthLoginConfigurer<HttpSecurity>
 ) {
+    @Value("\${auth.login-success-redirect-uri}") lateinit var loginSuccessUri: String
+
+    @Value("\${auth.login-failure-redirect-uri}") lateinit var loginFailureUri: String
+
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -20,7 +27,9 @@ class SecurityConfig(
             .cors { cors -> cors.disable() }
             .formLogin { form -> form.disable() }
             .httpBasic { basic -> basic.disable() }
-            .apply(gauth)
+            .apply(gauth
+                    .successHandler(SimpleUrlAuthenticationSuccessHandler(loginSuccessUri))
+                    .failureHandler(SimpleUrlAuthenticationFailureHandler(loginFailureUri)))
         return http.build()
     }
 }
