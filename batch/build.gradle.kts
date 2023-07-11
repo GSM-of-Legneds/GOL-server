3ef36035551530c24d5d4a35d315e90dd516302b
YangSijun528 <yangsijun5528@gmail.com>
2023-07-12 00:53:43 +0900

    spring 배치 관련 설정

--- batch/build.gradle.kts
+++ batch/build.gradle.kts
@@ -12,6 +12,8 @@ dependencies {
 
 	/* persistence */
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.mariadb.jdbc:mariadb-java-client") // batch metadata 저장
 
 	/* test */
 	testImplementation("org.springframework.security:spring-security-test")
