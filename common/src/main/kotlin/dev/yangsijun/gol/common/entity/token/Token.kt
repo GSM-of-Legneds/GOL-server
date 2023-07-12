4833a53052c508763a2901244e645e2882d2292a
YangSijun528 <yangsijun5528@gmail.com>
2023-07-12 16:46:53 +0900

    toString 추가

--- common/src/main/kotlin/dev/yangsijun/gol/common/entity/token/Token.kt
+++ common/src/main/kotlin/dev/yangsijun/gol/common/entity/token/Token.kt
@@ -1,6 +1,7 @@
 package dev.yangsijun.gol.common.entity.token
 
 import dev.yangsijun.gol.common.common.entity.BaseTimeEntity
+import dev.yangsijun.gol.common.common.util.GolObjectUtils
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
@@ -11,4 +12,6 @@ class Token(
     val gauthEmail: String,
     val accessToken: String,
     val refreshToken: String
-): BaseTimeEntity()
+): BaseTimeEntity() {
+    override fun toString() = GolObjectUtils.reflectionToString(this)
+}
