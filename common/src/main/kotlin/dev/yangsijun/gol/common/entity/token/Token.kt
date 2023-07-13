d5d30e01756ec26f65bc065c35589766ae9a7f78
YangSijun528 <yangsijun5528@gmail.com>
2023-07-13 11:50:17 +0900

    토큰 email 인덱스 추가

--- common/src/main/kotlin/dev/yangsijun/gol/common/entity/token/Token.kt
+++ common/src/main/kotlin/dev/yangsijun/gol/common/entity/token/Token.kt
@@ -4,12 +4,13 @@ import dev.yangsijun.gol.common.common.entity.BaseTimeEntity
 import dev.yangsijun.gol.common.common.util.GolObjectUtils
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
 class Token(
     @Id var id: ObjectId? = null,
-    val gauthEmail: String,
+    @Indexed val gauthEmail: String,
     val accessToken: String,
     val refreshToken: String
 ): BaseTimeEntity() {
